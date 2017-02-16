0945cbf965fc1ff15323216707b92280e837e50e
emrekaraca <creagas@googlemail.com>
2017-02-16 19:59:57 +0100

    Finished localStorage variant

--- server.js
+++ server.js
@@ -14,28 +14,33 @@ app.get('/set', function (req, res) {
     var value = req.query[key];
     localStorage.setItem(key, value);
     console.log("stored key: " + localStorage.getItem(key));
-    res.end("Your entry was successfully saved!");
+    res.end("Your entry was successfully saved!\nKey: " + key +"\n" + "Value: " + value);
   }
 });
 
 // Route to request a stored value
 app.get('/get', function (req, res) {
   console.log("The entered query is: " + req.query);
-  if (Object.keys(req.query).length !== 1) {
-    res.end("Please enter one key-value-pair!");
-  }
-  else if (req.query.hasOwnProperty('key') === false) {
+  if (Object.keys(req.query).length !== 1 || req.query.hasOwnProperty('key') === false) {
     res.end("Please enter the request in the following format: /get?key=[requested key]!")
   }
   else if (req.query.hasOwnProperty('key')) {
     console.log("requested key is: " + req.query.key);
     var requestedKey = req.query.key;
     if (localStorage.getItem(requestedKey) !== null) {
-      res.end("The requested value for the entered key " + requestedKey + " is: " + localStorage.getItem(requestedKey));
+      res.end("The requested value for the entered key \'" + requestedKey + "\' is: \'" + localStorage.getItem(requestedKey) + "\'.");
     } else {
-      res.end("The key " + requestedKey + " was not found!")
+      res.end("The key \'" + requestedKey + "\' was not found!")
     }
   }
 });
 
-app.listen(4000);
+// Route to reset storage
+app.get('/reset', function(req, res) {
+  localStorage.clear();
+  res.end("All entries were deleted!")
+})
+
+app.listen(4000, function() {
+  console.log("Up and running on 4000...");
+});
